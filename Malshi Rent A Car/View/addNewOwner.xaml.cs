6caec5fce40ec3882ca7a9fbae497769cb6cc28d
Bawantha Dissanayake <bawanthadissanayake@gmail.com>
2021-06-29 17:20:12 +0530

    add Owner

--- Malshi Rent A Car/View/addNewOwner.xaml.cs
+++ Malshi Rent A Car/View/addNewOwner.xaml.cs
@@ -23,5 +23,19 @@ namespace Malshi_Rent_A_Car
         {
             InitializeComponent();
         }
+
+        private void btn_save_Click(object sender, RoutedEventArgs e)
+        {
+            Owner owner = new Owner(txt_OwnFname.Text, txt_OwnLname.Text, txt_OwnNIC.Text, txt_OwnResAdrs.Text, txt_OwnWorkAdrs.Text, txt_OwnEmail.Text, Int32.Parse(txt_OwnTelMobile.Text), Int32.Parse(txt_OwnTelHome.Text),Int32.Parse(txt_OwnTelWork.Text), txt_OwnProfession.Text);
+            int i = owner.addOwner();
+            if (i==1)
+            {
+                MessageBox.Show("Data Saved Successfully!");
+            }
+            else
+            {
+                MessageBox.Show("Couldnt Save data.Please Try Again");
+            }
+        }
     }
 }
