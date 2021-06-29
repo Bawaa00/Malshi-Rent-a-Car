42eae945e8c37870325d4f76c5f489a720d1d295
Bawantha Dissanayake <bawanthadissanayake@gmail.com>
2021-06-29 15:51:04 +0530

    owner class created

---
+++ Malshi Rent A Car/Owner.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Malshi_Rent_A_Car
+{
+    class Owner
+    {
+        string fName;
+        string lName;
+        string NIC;
+        string homeAddress, offAddress;
+        string email;
+        int mobileContact, homeContact;
+        string profession;
+
+        public Owner(string fName, string lName, string NIC, string homeAddress, string offAddress, string email, int mobileContact, int homeContact, string profession)
+        {
+            this.fName = fName;
+            this.lName = lName;
+            this.NIC = NIC;
+            this.homeAddress = homeAddress;
+            this.offAddress = offAddress;
+            this.email = email;
+            this.mobileContact = mobileContact;
+            this.homeContact = homeContact;
+            this.profession = profession;
+        }
+
+        public void addOwner()
+        {
+
+        }
+    }
+
+}
