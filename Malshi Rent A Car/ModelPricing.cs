2d3dbe1215ec25a8963298237b1067353e10f0a9
Bawantha Dissanayake <bawanthadissanayake@gmail.com>
2021-06-30 17:54:47 +0530

    classes introduced

---
+++ Malshi Rent A Car/ModelPricing.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Malshi_Rent_A_Car
+{
+    class ModelPricing
+    {
+        string pID;
+        int vYear;
+        string vMake;
+        string vModel;
+        int longRent;
+        int shortRent;
+        float extraMilageCost;
+    }
+}
---
+++ Malshi Rent A Car/Vehicle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Malshi_Rent_A_Car
+{
+    class Vehicle
+    {
+        string plateNumber;
+        string vType;
+        string vColor;
+        string vPhoto;
+        string transmission;
+        int engineCapacity;
+        int noOfPassengers;
+        string licenseStartDate;
+        string licenseExpiryDate;
+        string insExpiryDate;
+        string insStartDate;
+        string fuelType;
+        bool bookingStatus;
+        string lendingDate;
+        int monthlyPay;
+        string wName;
+        string wAddress;
+        int wContact;
+    }
+}
