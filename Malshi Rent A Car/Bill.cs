d68b1ad6bdec7458cb0a991e5556dc6b728a9dd8
Bawantha Dissanayake <bawanthadissanayake@gmail.com>
2021-07-04 18:11:28 +0530

    classes introduced

---
+++ Malshi Rent A Car/Bill.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Malshi_Rent_A_Car
+{
+    class Bill
+    {
+        string billID;
+        string billDate;
+        int extraCost;
+        string payMethod;
+
+        public Bill(string id,string date,int cost,string method)
+        {
+            billID = id;
+            billDate = date;
+            extraCost = cost;
+            payMethod = method;
+        }
+        public Bill()
+        {
+
+        }
+    }
+}
--- Malshi Rent A Car/View/AddNewBooking.xaml.cs
+++ Malshi Rent A Car/View/AddNewBooking.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Data;
 
 namespace Malshi_Rent_A_Car
 {
@@ -22,6 +23,23 @@ namespace Malshi_Rent_A_Car
         public AddNewBooking()
         {
             InitializeComponent();
+        }
+        DataTable dt = new DataTable();
+        Vehicle vehicle = new Vehicle();
+        Customer customer = new Customer();
+        Booking book = new Booking();
+
+        private void form_addBooking_Loaded(object sender, RoutedEventArgs e)
+        {
+            dt = vehicle.viewVehicle();
+            cmb_vLplate.ItemsSource = dt.DefaultView;
+            cmb_vLplate.DisplayMemberPath = "Plate No";
+            cmb_vLplate.SelectedValuePath = "Plate No";
+
+            dt = vehicle.viewVehicle();
+            cmb_vLplate.ItemsSource = dt.DefaultView;
+            cmb_vLplate.DisplayMemberPath = "Plate No";
+            cmb_vLplate.SelectedValuePath = "Plate No";
         }
     }
 }
