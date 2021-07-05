8a4391c94fcea16454dfaab8f90fff9e7b83ea5b
Bawantha Dissanayake <bawanthadissanayake@gmail.com>
2021-07-05 13:21:14 +0530

    pricing

--- Malshi Rent A Car/ModelPricing.cs
+++ Malshi Rent A Car/ModelPricing.cs
@@ -10,6 +10,7 @@ namespace Malshi_Rent_A_Car
     class ModelPricing
     {
         string pID;
+        string vType;
         int vYear;
         string vMake;
         string vModel;
@@ -17,23 +18,49 @@ namespace Malshi_Rent_A_Car
         int shortRent;
         float extraMilageCost;
         Database db = new Database();
+        DataTable dt = new DataTable();
 
         public ModelPricing()
         {
         }
+        public ModelPricing(string pID,string vType,int vYear,string vMake,string vModel,int shortRent,int longRent,float extraMilageCost)
+        {
+            this.pID = pID;
+            this.vType = vType;
+            this.vYear = vYear;
+            this.vMake = vMake;
+            this.vModel = vModel;
+            this.shortRent = shortRent;
+            this.longRent = longRent;
+            this.extraMilageCost = extraMilageCost;
+        }
 
         public DataTable viewPricing()
-        {
-            DataTable dt = new DataTable();
+        {            
             dt = db.getData("exec view_pricing");
             return dt;
         }
 
         public DataTable viewPricing(string modelID)
         {
-            DataTable dt = new DataTable();
             dt = db.getData("exec view_pricing_id '"+modelID+"'");
             return dt;
+        }
+
+        public int addPricing()
+        {
+            int i = db.save_update_delete("insert into Pricing values ('" + pID + "','" + vType + "','" + vYear + "','" + vMake + "','" + vModel + "','" + shortRent + "','" + longRent + "','" + extraMilageCost + "')");
+            return i;
+        }
+        public int updatePricing()
+        {
+            int i = db.save_update_delete("update Pricing set vType='" + vType + "', vYear ='" + vYear + "', vMake = '" + vMake + "',vModel = '" + vModel + "',shortRent = '" + shortRent + "',longRent = '" + longRent + "',extraMilageCost='" + extraMilageCost + "' where pID='" + pID + "'");
+            return i;
+        }
+        public int deletePricing(string pid)
+        {
+            int i = db.save_update_delete("delete from Pricing where pID = '" + pid + "'");
+            return i;
         }
 
     }
