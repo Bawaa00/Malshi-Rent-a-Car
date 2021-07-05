e8670079315f14967addf495a4d931cd2bc9ad45
Thuseetha Ratnayake <codse192f-017@student.nibm.lk>
2021-07-05 11:37:00 +0530

    ValidateUpdateCustomer

--- Malshi Rent A Car/View/UpdateCustomer.xaml.cs
+++ Malshi Rent A Car/View/UpdateCustomer.xaml.cs
@@ -15,6 +15,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Data;
 using Microsoft.Win32;
+using System.Text.RegularExpressions;
 
 namespace Malshi_Rent_A_Car
 {
@@ -47,52 +48,79 @@ namespace Malshi_Rent_A_Car
 
         private void cmb_nic_DropDownClosed(object sender, EventArgs e)
         {
-            dt = customer.viewCustomerNIC(cmb_nic.Text);
-
-           // cmb_nic.Text = dt.Rows[0][0].ToString();
-            txt_firstname.Text = dt.Rows[0][1].ToString();
-            txt_lname.Text = dt.Rows[0][2].ToString();
-            txt_CusResAdrs.Text = dt.Rows[0][3].ToString();
-            txt_CusWorkAdrs.Text = dt.Rows[0][8].ToString();
-            txt_CusTelHome.Text = dt.Rows[0][5].ToString();
-            txt_CusTelMobile.Text = dt.Rows[0][4].ToString();
-            txt_CusTelWork.Text = dt.Rows[0][9].ToString();
-            txt_CusEmail.Text = dt.Rows[0][6].ToString();
-            txt_CusProfession.Text = dt.Rows[0][7].ToString();
-            path = dt.Rows[0][10].ToString();
-            txt_CusKinName.Text = dt.Rows[0][11].ToString();
-            txt_CusKinkAdrs.Text = dt.Rows[0][12].ToString();
-            txt_CusKinConatct.Text = dt.Rows[0][13].ToString();
-
-            if (path != "")
+            if (cmb_nic.SelectedItem == null)
             {
-                BitmapImage image = new BitmapImage();
-                image.BeginInit();
-                image.CacheOption = BitmapCacheOption.OnLoad;
-                image.UriSource = new Uri(path);
-                image.EndInit();
-                img_customer.Source = image;
+                error_msg.Text = "Please Select NIC";
             }
+
             else
-            {
-                img_customer.Source = null;
-            }
+            { dt = customer.viewCustomerNIC(cmb_nic.Text);
 
+                // cmb_nic.Text = dt.Rows[0][0].ToString();
+                txt_firstname.Text = dt.Rows[0][1].ToString();
+                txt_lname.Text = dt.Rows[0][2].ToString();
+                txt_CusResAdrs.Text = dt.Rows[0][3].ToString();
+                txt_CusWorkAdrs.Text = dt.Rows[0][8].ToString();
+                txt_CusTelHome.Text = dt.Rows[0][5].ToString();
+                txt_CusTelMobile.Text = dt.Rows[0][4].ToString();
+                txt_CusTelWork.Text = dt.Rows[0][9].ToString();
+                txt_CusEmail.Text = dt.Rows[0][6].ToString();
+                txt_CusProfession.Text = dt.Rows[0][7].ToString();
+                path = dt.Rows[0][10].ToString();
+                txt_CusKinName.Text = dt.Rows[0][11].ToString();
+                txt_CusKinkAdrs.Text = dt.Rows[0][12].ToString();
+                txt_CusKinConatct.Text = dt.Rows[0][13].ToString();
+
+                if (path != "")
+                {
+                    BitmapImage image = new BitmapImage();
+                    image.BeginInit();
+                    image.CacheOption = BitmapCacheOption.OnLoad;
+                    image.UriSource = new Uri(path);
+                    image.EndInit();
+                    img_customer.Source = image;
+                }
+                else
+                {
+                    img_customer.Source = null;
+                }
+            }
 
         }
 
         private void btn_update_Click(object sender, RoutedEventArgs e)
         {
-            string name = System.IO.Path.GetFileName(path);
-            string destinationPath = GetDestinationPath(name);
-            File.Copy(path, destinationPath, true);
-            Customer upCustomer = new Customer(txt_firstname.Text, txt_lname.Text, cmb_nic.Text, txt_CusEmail.Text, txt_CusResAdrs.Text, Int32.Parse(txt_CusTelHome.Text), Int32.Parse(txt_CusTelMobile.Text), txt_CusProfession.Text, txt_CusWorkAdrs.Text, Int32.Parse(txt_CusTelWork.Text), txt_CusKinName.Text, txt_CusKinkAdrs.Text, Int32.Parse(txt_CusKinConatct.Text), destinationPath); ;
-            int i = upCustomer.updateCustomer();
-            if (i ==1)
-                MessageBox.Show("Data save Successfully", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+            try
+            {
+                string name = System.IO.Path.GetFileName(path);
+                string destinationPath = GetDestinationPath(name);
+                File.Copy(path, destinationPath, true);
+                Customer upCustomer = new Customer(txt_firstname.Text, txt_lname.Text, cmb_nic.Text, txt_CusEmail.Text, txt_CusResAdrs.Text, Int32.Parse(txt_CusTelHome.Text), Int32.Parse(txt_CusTelMobile.Text), txt_CusProfession.Text, txt_CusWorkAdrs.Text, Int32.Parse(txt_CusTelWork.Text), txt_CusKinName.Text, txt_CusKinkAdrs.Text, Int32.Parse(txt_CusKinConatct.Text), destinationPath); ;
+                int i = upCustomer.updateCustomer();
+                if (i == 1)
+                    MessageBox.Show("Data save Successfully", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
 
-            else
-                MessageBox.Show("Data cannot save", "error", MessageBoxButton.OK, MessageBoxImage.Error);
+                else
+                    MessageBox.Show("Data cannot save", "error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            catch (ArgumentNullException)
+            {
+                MessageBox msg = new MessageBox();
+                msg.errorMsg("Please upload a photo");
+                msg.Show();
+            }
+            catch (System.Data.SqlClient.SqlException)
+            {
+                MessageBox msg = new MessageBox();
+                msg.errorMsg("Please fill the form correctly. Database Error");
+                msg.Show();
+            }
+            catch (Exception ex)
+            {
+                MessageBox msg = new MessageBox();
+                msg.errorMsg("Oops something went worng. " + ex.Message);
+                msg.Show();
+            }
         }
 
         private void bttn_delete_Click(object sender, RoutedEventArgs e)
@@ -123,16 +151,25 @@ namespace Malshi_Rent_A_Car
 
         private void btn_upload_Click(object sender, RoutedEventArgs e)
         {
-            OpenFileDialog open = new OpenFileDialog();
-            open.Multiselect = false;
-            open.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp)|*.jpg; *.jpeg; *.gif; *.bmp";
-            bool? result = open.ShowDialog();
-
-            if (result == true)
+            try
             {
-                path = open.FileName; // Stores Original Path in Textbox    
-                ImageSource imgsource = new BitmapImage(new Uri(path)); // Just show The File In Image when we browse It
-                img_customer.Source = imgsource;
+                OpenFileDialog open = new OpenFileDialog();
+                open.Multiselect = false;
+                open.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp)|*.jpg; *.jpeg; *.gif; *.bmp";
+                bool? result = open.ShowDialog();
+
+                if (result == true)
+                {
+                    path = open.FileName; // Stores Original Path in Textbox    
+                    ImageSource imgsource = new BitmapImage(new Uri(path)); // Just show The File In Image when we browse It
+                    img_customer.Source = imgsource;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox msg = new MessageBox();
+                msg.errorMsg("Oops soomething went worng. " + ex.Message);
+                msg.Show();
             }
         }
 
@@ -142,6 +179,116 @@ namespace Malshi_Rent_A_Car
             cmb_nic.ItemsSource = dt.DefaultView;
             cmb_nic.DisplayMemberPath = "NIC";
             cmb_nic.SelectedValuePath = "NIC";
+        }
+
+        private void txt_firstname_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (txt_firstname.Text.Length == 0)
+                error_msg.Text = "Please Enter Customer First Name  ";
+            else
+                error_msg.Text = "";
+        }
+
+        private void txt_lname_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+            if (txt_lname.Text.Length == 0)
+                error_msg.Text = "Please Enter Custoer last Name  ";
+            else
+                error_msg.Text = "";
+        }
+
+        private void txt_CusEmail_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (txt_CusEmail.Text.Length == 0)
+                error_msg.Text = "Please Enter Custoer Email  ";
+            else
+                error_msg.Text = "";
+        }
+
+        private void txt_CusResAdrs_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (txt_CusResAdrs.Text.Length == 0)
+                error_msg.Text = "Please Enter Custoer Home Addrss  ";
+            else
+                error_msg.Text = "";
+        }
+
+        private void txt_CusTelHome_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (txt_CusTelHome.Text.Length == 0)
+                error_msg.Text = "Please Enter Customer  Home Telephone ";
+            else if (!Regex.IsMatch(txt_CusTelHome.Text, @"^(?:7|0|(?:\+94))[0-9]{8,9}$"))
+
+                error_msg.Text = "Contact No not Valid";
+            else
+                error_msg.Text = "";
+        }
+
+        private void txt_CusTelMobile_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (txt_CusTelMobile.Text.Length == 0)
+                error_msg.Text = "Please Enter Customer Mobile Number ";
+            else if (!Regex.IsMatch(txt_CusTelMobile.Text, @"^(?:7|0|(?:\+94))[0-9]{8,9}$"))
+
+                error_msg.Text = "Contact No not Valid";
+            else
+                error_msg.Text = "";
+
+        }
+
+        private void txt_CusProfession_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (txt_CusProfession.Text.Length == 0)
+                error_msg.Text = "Please Enter Custoer Profession  ";
+            else
+                error_msg.Text = "";
+        }
+
+        private void txt_CusWorkAdrs_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (txt_CusWorkAdrs.Text.Length == 0)
+                error_msg.Text = "Please Enter Custoer Work Address  ";
+            else
+                error_msg.Text = "";
+        }
+
+        private void txt_CusTelWork_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (txt_CusTelWork.Text.Length == 0)
+                error_msg.Text = "Please Enter Customer Work Telephone Number ";
+            else if (!Regex.IsMatch(txt_CusTelWork.Text, @"^(?:7|0|(?:\+94))[0-9]{8,9}$"))
+
+                error_msg.Text = "Contact No not Valid";
+            else
+                error_msg.Text = "";
+        }
+
+        private void txt_CusKinName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (txt_CusKinName.Text.Length == 0)
+                error_msg.Text = "Please Enter Custoer Kin Name ";
+            else
+                error_msg.Text = "";
+        }
+
+        private void txt_CusKinkAdrs_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (txt_CusKinkAdrs.Text.Length == 0)
+                error_msg.Text = "Please Enter Custoer Kin Address ";
+            else
+                error_msg.Text = "";
+        }
+
+        private void txt_CusKinConatct_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (txt_CusKinConatct.Text.Length == 0)
+                error_msg.Text = "Please Enter Customer Kin Contact Number ";
+            else if (!Regex.IsMatch(txt_CusKinConatct.Text, @"^(?:7|0|(?:\+94))[0-9]{8,9}$"))
+
+                error_msg.Text = "Contact No not Valid";
+            else
+                error_msg.Text = "";
         }
     }
 }
