3386ae0580aab81957b02d814d86c5dd0ed93935
Bawantha Dissanayake <bawanthadissanayake@gmail.com>
2021-07-06 20:29:52 +0530

    dashboard items added

--- Malshi Rent A Car/View/Dashboard.xaml.cs
+++ Malshi Rent A Car/View/Dashboard.xaml.cs
@@ -11,6 +11,10 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using LiveCharts;
+using LiveCharts.Wpf;
+using LiveCharts.Charts;
+using System.Data;
 
 namespace Malshi_Rent_A_Car
 {
@@ -24,10 +28,62 @@ namespace Malshi_Rent_A_Car
             InitializeComponent();
         }
         String utype;
+        Vehicle vehicle = new Vehicle();
         public Dashboard(string utype)
         {
             InitializeComponent();
             this.utype = utype;
+        }
+        public Func<ChartPoint, string> PointLabel { get; set; }
+
+        public void PieChart()
+        {
+            PointLabel = chartPoint => string.Format("{0}({1:P})", chartPoint.Y, chartPoint.Participation);
+            DataContext = this;
+        }
+
+        public void labelData()
+        {
+
+           /* string path;
+            DataTable dt = new DataTable();
+            dt = db.getData("select COUNT(L_Plate) from Vehicle");
+            label_vehicle.Content = dt.Rows[0][0].ToString();
+            dt = db.getData("select COUNT(D_ID) from Driver");
+            label_driver.Content = dt.Rows[0][0].ToString();
+            dt = db.getData("select COUNT(Cus_ID) from Customer");
+            label_customer.Content = dt.Rows[0][0].ToString();
+
+            try
+            {
+                string lnum;
+                dt = db.getData("select L_Plate,COUNT(L_Plate) from Car_Booking,Vehicle where L_Plate = VNO group by L_Plate");
+                lnum = dt.Rows[0][0].ToString();
+                dt = db.getData("select * from Vehicle where L_Plate = '" + lnum + "'");
+                lbl_year.Content = dt.Rows[0][1].ToString();
+                lbl_make.Content = dt.Rows[0][2].ToString();
+                lbl_model.Content = dt.Rows[0][3].ToString();
+                lbl_cat.Content = dt.Rows[0][4].ToString();
+                path = dt.Rows[0][13].ToString();
+                BitmapImage image = new BitmapImage();
+                image.BeginInit();
+                image.CacheOption = BitmapCacheOption.OnLoad;
+                image.UriSource = new Uri(path);
+                image.EndInit();
+                img_vehicle.Source = image;
+            }
+            catch (IndexOutOfRangeException)
+            {
+               Messagebox obj = new Messagebox();
+                obj.errorMsg("Database Error");
+                obj.Show();
+            }
+            catch (Exception ex)
+            {
+                Messagebox msg = new Messagebox();
+                msg.errorMsg("Oops soomething went worng. " + ex.Message);
+                msg.Show();
+            }*/
         }
 
         private void list_customer_Selected(object sender, RoutedEventArgs e)
@@ -64,6 +120,12 @@ namespace Malshi_Rent_A_Car
         {
             SearchViewVehicle obj = new SearchViewVehicle();
             obj.Show();
+        }
+
+        private void form_dashboard_Loaded(object sender, RoutedEventArgs e)
+        {
+            txt_totV.Text=vehicle.getTotalVehicleCount().ToString();
+            txt_available.Text = vehicle.getAvaialableVehicleCount().ToString();
         }
     }
 }
